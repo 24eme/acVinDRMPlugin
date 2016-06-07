5475bee7063041f2f6f8e43d6f991ad41f735e5c
Vincent LAURENT <vince.laurent@gmail.com>
2016-06-07 15:08:38 +0200

    Adaptation de popup vrac à généricité de gestion des détails

--- lib/actions/drmGeneriqueActions.class.php
+++ lib/actions/drmGeneriqueActions.class.php
@@ -69,4 +69,37 @@ class drmGeneriqueActions extends sfActions {
         return $this->mouvementsByProduit;
     }
 
+    protected function processProduitDetails($request, $formClass) {
+        $this->detail = $this->getRoute()->getDRMDetail();
+        $this->drm = $this->detail->getDocument();
+        $this->isTeledeclarationMode = $this->isTeledeclarationDrm();
+        $this->catKey = $request->getParameter('cat_key');
+        $this->key = $request->getParameter('key');
+        $this->form = new $formClass($this->detail->get($this->catKey)->get($this->key."_details"), array(),  array('isTeledeclarationMode' => $this->isTeledeclarationMode && false));
+
+        if ($request->isMethod(sfRequest::POST)) {
+            $this->form->bind($request->getParameter($this->form->getName()));
+
+            if($this->form->isValid()) {
+                $this->form->update();
+                $this->drm->update();
+                $this->drm->save();
+
+                if($request->isXmlHttpRequest())
+                {
+                    $this->getUser()->setFlash("notice", 'Le détail a été mis à jour avec success.');
+
+                    return $this->renderText(json_encode(array("success" => true, "type" => $this->catKey."_".$this->key, "volume" => $this->detail->get($this->catKey)->get($this->key), "document" => array("id" => $this->drm->get('_id'),"revision" => $this->drm->get('_rev')))));
+                }
+
+                return $this->redirect('drm_edition_detail', $this->detail);
+            }
+            if($request->isXmlHttpRequest())
+            {
+
+                return $this->renderText(json_encode(array('success' => false ,'content' => $this->getPartial('formContent', array('form' => $this->form, 'detail' => $this->detail,'isTeledeclarationMode' => $this->isTeledeclarationMode, 'catKey' => $catKey, 'key' => $key)))));
+            }
+        }
+    }
+
 }
--- modules/drm_export_details/actions/actions.class.php
+++ modules/drm_export_details/actions/actions.class.php
@@ -4,31 +4,7 @@ class drm_export_detailsActions extends drmGeneriqueActions
 {
 
     public function executeProduit(sfWebRequest $request) {
-        $this->detail = $this->getRoute()->getDRMDetail();
-        $this->drm = $this->detail->getDocument();
-        $this->isTeledeclarationMode = $this->isTeledeclarationDrm();
-        $this->catKey = $request->getParameter('cat_key');
-        $this->key = $request->getParameter('key');
-        $this->form = new DRMDetailExportForm($this->detail->get($this->catKey)->get($this->key."_details"), array(),  array('isTeledeclarationMode' => $this->isTeledeclarationMode && false));
 
-        if ($request->isMethod(sfRequest::POST)) {
-            $this->form->bind($request->getParameter($this->form->getName()));
-            if($this->form->isValid()) {
-                $this->form->update();
-                $this->drm->update();
-                $this->drm->save();
-                if($request->isXmlHttpRequest())
-                {
-                    $this->getUser()->setFlash("notice", 'Le détail des exports a été mis à jour avec success.');
-                    return $this->renderText(json_encode(array("success" => true, "type" => $this->catKey."_".$this->key, "volume" => $this->detail->get($this->catKey)->get($this->key), "document" => array("id" => $this->drm->get('_id'),"revision" => $this->drm->get('_rev')))));
-                }
-
-                return $this->redirect('drm_edition_detail', $this->detail);
-            }
-            if($request->isXmlHttpRequest())
-            {
-                return $this->renderText(json_encode(array('success' => false ,'content' => $this->getPartial('formContent', array('form' => $this->form, 'detail' => $this->detail,'isTeledeclarationMode' => $this->isTeledeclarationMode, 'catKey' => $catKey, 'key' => $key)))));
-            }
-        }
+        return $this->processProduitDetails($request, "DRMDetailExportForm");
     }
 }
